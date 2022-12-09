c18399da6cd3789a94e85035baa1659d49d2d53e
donetian-petkov <donetian+petkov@gmail.com>
2022-12-09 22:46:44 +0200

    Initial Database Configuration

--- HouseManagementApp.Infrastructure/Data/ApplicationDbContext.cs
+++ HouseManagementApp.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace HouseManagementApp.Data;
+namespace HouseManagementApp.Infrastructure.Data;
 
 public class ApplicationDbContext : IdentityDbContext
 {
--- HouseManagementApp/Program.cs
+++ HouseManagementApp/Program.cs
@@ -1,13 +1,18 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using HouseManagementApp.Data;
+using HouseManagementApp.Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var connectionString = builder.Configuration["MySQLDatabase:ConnectionString"];
+
+var serverVersion = new MySqlServerVersion(new Version(5, 7, 39));
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(connectionString));
+{
+    options.UseMySql(connectionString,serverVersion);
+});
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
