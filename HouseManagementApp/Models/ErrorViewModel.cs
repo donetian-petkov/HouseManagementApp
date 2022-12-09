28672bbd4c8a0f19c947f44a1ee7601dbc1205d4
donetian-petkov <donetian+petkov@gmail.com>
2022-12-09 22:24:08 +0200

    Initial Commit

---
+++ HouseManagementApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace HouseManagementApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
