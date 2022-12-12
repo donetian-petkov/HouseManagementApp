1fc214c810c9bfaa538a06311a30ea5831c6c647
donetian-petkov <donetian+petkov@gmail.com>
2022-12-12 17:24:44 +0200

    Administration Area Initial Commit

---
+++ HouseManagementApp/Areas/Administration/Views/Home/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HouseManagementApp.Areas.Administration.Views.Home;
+
+public class Index : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
